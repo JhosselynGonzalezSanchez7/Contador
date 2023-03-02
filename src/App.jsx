da59dc809c2f72352a7918bdc69a2ce463d703f8
JhosselynGonzalezSanchez7 <l19350296@tuxtepec.tecnm.mx>
2023-03-01 20:47:25 -0600

    actualizacion botones decremendo y restablecer del contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
 import { useState } from "react";
 
-
 function App() {
-  //let contador = 0;
-  const [contador, setContador] = useState(0)
+  //let Contador = 0;
+  const [Contador, setContador] = useState(0)
   
+  const Incremento = () => {
+    setContador(Contador+1);
+  }
+
+  const Restablecer = () => {
+    setContador(0);
+  }
+
+  const Decremento = () => {
+    setContador(Contador-1);
+  }
+ 
   return (
     <div>
-    <h3>Contador</h3>
-    <hr />
-    <h1>{contador}</h1>
-    <button 
-    onClick={()=>{
-      setContador(contador + 1);
-      console.log (contador)
-    }}
-    type = "button" 
-    className="btn btn-primary"
-    >+1</button>
+      <h3>Contador</h3>
+      <hr />
+      <h1>{Contador}</h1>
+      <button onClick = {
+        Incremento
+      }
+      type="button" 
+      className="btn btn-primary"
+      >+1</button>
+      
+      < button onClick = {
+        Restablecer
+      }
+      type="button" 
+      className="btn btn-secondary"
+      >Restablecer</button>
+
+      < button onClick = {
+        Decremento
+      }
+      type="button" 
+      className="btn btn-success"
+      >-1</button>
     </div>
   )
 }
